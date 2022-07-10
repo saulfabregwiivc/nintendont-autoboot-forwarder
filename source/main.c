47e0246083695c5061249a201581788481f34b21
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2022-07-10 18:10:50 -0500

    Fix some mistakes

--- source/main.c
+++ source/main.c
@@ -68,24 +68,24 @@ int main(int argc, char *argv[])
 	ICInvalidateRange(BOOTER_ADDR,app_booter_bin_size);
 
 	char *CMD_ADDR = (char*)ARGS_ADDR + sizeof(struct __argv);
-	const char *romDir = ROM_DIR;
-	const char *romFile = ROM_FILE;
+	const char *argument1 = ARGUMENT_1;
+	const char *argument2 = ARGUMENT_2;
 	const size_t full_fPath_len = strlen(fPath)+1;
-	const size_t full_romDir_len = strlen(romDir)+1;
-	const size_t full_romFile_len = strlen(romFile)+1;
+	const size_t full_argument1_len = strlen(argument1)+1;
+	const size_t full_argument2_len = strlen(argument2)+1;
 	const size_t full_args_len = sizeof(struct __argv)+full_fPath_len+full_romDir_len+full_romFile_len;
 
 	memset(ARGS_ADDR, 0, full_args_len);
 	ARGS_ADDR->argvMagic = ARGV_MAGIC;
 	ARGS_ADDR->commandLine = CMD_ADDR;
-	ARGS_ADDR->length = full_fPath_len+full_romDir_len+full_romFile_len;
+	ARGS_ADDR->length = full_fPath_len+full_argument1_len+full_argument2_len;
 	ARGS_ADDR->argc = 3;
 
 	memcpy(CMD_ADDR, fPath, full_fPath_len);
 	CMD_ADDR += full_fPath_len;
-	memcpy(CMD_ADDR, romDir, full_romDir_len);
-	CMD_ADDR += full_romDir_len;
-	memcpy(CMD_ADDR, romFile, full_romFile_len);
+	memcpy(CMD_ADDR, argument1, full_argument1_len);
+	CMD_ADDR += full_argument1_len;
+	memcpy(CMD_ADDR, argument2, full_argument2_len);
 	DCFlushRange(ARGS_ADDR, full_args_len);
 
 	//possibly affects nintendont speed?
